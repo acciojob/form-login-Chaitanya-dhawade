07012a441ae6ba627274624aa387808720be72b4
Chaitanya-dhawade <chaitanyadhawade23@gmail.com>
2026-06-16 23:03:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event){
+
+    event.preventDefault();
+
+    const fname = document.querySelector('input[name="fname"]').value.trim();
+    const lname = document.querySelector('input[name="lname"]').value.trim();
+
+    alert(fname + " " + lname);
 
 }
